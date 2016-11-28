45b87ba4e098b37b7f36a873f8613a7cb35f2a76
Vita <vitac215@gmail.com>
2016-11-27 20:12:07 -0500

    another approach: delete and add ok

--- js/tree.js
+++ js/tree.js
@@ -6,12 +6,26 @@ var margin = {top: 50, right: 120, bottom: 20, left: 300},
 var tree = d3.layout.tree()
         .size([height, width]);
 
-var root = {"name":"Concept"},
-        nodes = tree(root);
+var root = {
+    "name": "Concept",
+    "id": 1
+    };
 
-root.parent = null;
-root.px = root.x;  // previous x = current y
-root.py = root.y;  // previous y = current y
+var nodes = tree(root);
+
+// Check
+var id = ++nodes.length; 
+
+// Set the parent x and y for all nodes, check
+nodes.forEach( function(node) {
+    if (node.id == 1) {
+        node.px = node.x = 500;
+        node.py = node.y = 304;
+    } else {
+        node.px = node.parent.x;
+        node.py = node.parent.y;
+    }
+});
 
 var diagonal = d3.svg.diagonal()
     .projection(function(d) { 
@@ -28,134 +42,194 @@ var node = svg.selectAll(".node");
 var link = svg.selectAll(".link");
 
 
-update();
+update(root);
 
 
-function update() {
+function update(root) {
 
-    // Assign nodes
+    var duration1 = 500;
+    var duration2 = 1000;
+
+    // Assign nodes and links
+    var node = svg.selectAll(".node"); 
+    var link = svg.selectAll(".link");
+
     var nodes = tree.nodes(root);
+    var links = tree.links(nodes);
 
     // Define depth and id
     node = node.data(nodes, function(d) { 
         d.y = d.depth * 100; 
         return d.id || (d.id = ++i); 
     });
-
-    // Enter the nodes
-    var nodeEnter = node.enter().append("g")
-            .attr("class", "node")
-            .attr("id", function(d){
-                return d.id;
-            })
-            .attr("parent", function(d){
-                return d.parent ? d.parent.id : null;
-                // (condition) ? [true path] : [false path];
-            })
-            .attr("transform", function(d) { 
-                return "translate(" + d.x + "," + d.y + ")"; 
-            });
-
-    // Append nodes
-    nodeEnter.append("circle")
-            .attr("r", 15)
-            .style("fill", "#fff");
-
-    // Append minus button
-    nodeEnter.append("circle")
-            .attr("class", "plus")
-            .attr("cx", "-18px")
-            .attr("cy", "18px")
-            .attr("r", 10)
-            .style("fill", "#1F800C")
-            .call(add_node, "name");
-
-    // Append plus button
-    nodeEnter.append("circle")
-            .attr("class", "minus")
-            .attr("cx", "18px")
-            .attr("cy", "18px")
-            .attr("r", 10)
-            .style("fill", "#801f0c")
-            .call(delete_node);
-
-    // Append label text
-    nodeEnter.append("text")
-            .attr("y", -25)
-            .attr("dy", ".35em")
-            .attr("text-anchor", "middle")
-            .text(function(d) { 
-                return d.name; 
-            })
-            .style("fill-opacity", 1)
-            .call(make_editable, "name");
-
-
-    // Assign links
-    var links = tree.links(nodes);
-
-    // Define link
     link = link.data(links, function(d) { 
         return d.source.id + "-" + d.target.id; 
     });
 
-    // Append links from the parent’s old position.
-    link.enter().insert("path", ".node")
-            .attr("class", "link")
-            .attr("d", diagonal);
+    // Set node
+    var nodesEnter = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";  
+        })
+
+    // Add label text
+    nodesEnter.append('g')
+        .attr('class', 'label')
+        .append('text')
+        .attr("y", -23)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(function(d) { 
+            return d.name; 
+        })
+        .call(make_editable, function(d) { 
+            return d.name; 
+        });
+
+    // Add circles
+    var circlesGroup = nodesEnter.append('g')
+        .attr('class','circles');
+
+    // Add main circle
+    var mainCircles = circlesGroup.append("circle")
+        .attr('class','main')
+        .attr("r", 15);
+
+    circlesGroup.append("circle")
+        .attr('class','delete')
+        .attr('cx', 0)
+        .attr('cy', 0)
+        .attr('fill','#801f0c')
+        .attr('opacity',0.8)
+        .attr("r", 0);
+
+    circlesGroup.append("circle")
+        .attr('class','add')
+        .attr('cx', 0)
+        .attr('cy', 0)
+        .attr('fill','#1f800c')
+        .attr('opacity',0.8)
+        .attr("r", 0);  
+
+    // Hover onto the node, display add and delete buttons
+    circlesGroup.on("mouseenter", function() {
+        var elem = this.__data__; 
+        elem1 = d3.selectAll(".delete").filter(function(d, i) { 
+            return elem.id == d.id ? this : null;
+            // (condition) ? [true path] : [false path];
+        });
+        elem2 = d3.selectAll(".add").filter(function(d, i) { 
+            return elem.id == d.id ? this : null;
+        });
+
+        elem2.transition()
+          .duration(duration1)
+          .attr('cx', -20)
+          .attr('cy', 20)
+          .attr("r", 8); 
+
+        elem1.transition()
+          .duration(duration1)
+          .attr('cx', 20)
+          .attr('cy', 20)
+          .attr("r", 8); 
+    }); 
+
+    circlesGroup.on("mouseleave", function() {
+        var elem = this.__data__; 
+        elem1 = d3.selectAll(".delete").filter(function(d,i) { 
+            return elem.id == d.id ? this : null;
+        });
+        elem2 = d3.selectAll(".add").filter(function(d,i) { 
+            return elem.id == d.id ? this : null;
+        });
+
+        elem2.transition()
+          .duration(duration1)
+          .attr('cy', 0)
+          .attr('cx', 0)
+          .attr("r", 0); 
+          
+        elem1.transition()
+          .duration(duration1)
+          .attr('cy', 0)
+          .attr('cx', 0)
+          .attr("r", 0); 
+    }); 
+
+
+    // Set links
+    // Add links from the parent's old position
+    var linkEnter = link.enter()
+      .insert("path", '.node')
+      .attr("class", "link")
+      .attr("d", diagonal);
 
     // Link new nodes to new position
-    var trans = svg.transition();
+    var trans = svg.transition().duration(duration2);
     trans.selectAll(".link")
-            .attr("d", diagonal);
+        .attr("d", diagonal);
     trans.selectAll(".node")
-            .attr("transform", function(d) {
-                d.px = d.x; 
-                d.py = d.y; 
-                return "translate(" + d.x + "," + d.y + ")"; 
-            });
+        .attr("transform", function(d) {
+            d.px = d.x; 
+            d.py = d.y; 
+            return "translate(" + d.x + "," + d.y + ")"; 
+        });
 
+    // Delete node
+    node.select('.delete').on('click', function() {
+        var p = this.__data__; 
+        if(p.id != 1) {
+            deleteNode(p);
+            var childArray = p.parent.children;
+            childArray = childArray.splice(childArray.indexOf(p), 1);
+
+            update(root);
+        }
+        function deleteNode(p) {
+            if (!p.children) {
+                if (p.id) { 
+                    p.id = null; 
+                }
+                return p; 
+            } else {
+                for(var i = 0; i < p.children.length; i++) {
+                    p.children[i].id == null; 
+                    deleteNode(p.children[i]); 
+                }
+                p.children = null;
+                return p; 
+            }
+        }
+    }); 
+
+
+    // Add node
+    node.select('.add').on('click', function() { 
+        var p = this.__data__;
+        var addedId = id++; 
+        var d = {name: 'Sub' + addedId};  
+        d.id = addedId;
+        if (p.children) { 
+            p.children.push(d); 
+        } else{ 
+            p.children = [d]; 
+        } 
+        d.px = p.x;
+        d.py = p.x;
+        d3.event.preventDefault();
+
+        update(root);
+    });
+
+    node.exit().remove(); 
+    link.exit().remove();
+
+    link.transition()
+    .duration(duration2).attr("d", diagonal); 
 }
 
-// function add_node(d) {
-//     this.on("click", function(d) {
-//         if (d.id != 1) {
-//             var newNode = {"name": "Sub-concepts"};
-//             var p = nodes[0];
-//             if (d.children) {
-//                 d.children.push(newNode);
-//             } else {
-//                 d.children = [newNode];
-//             }
-//             update();
-//         }
-//     });
-// }
-
-// function delete_node(d) {
-//     this.on("click", function(d){
-//         console.log(d);
-//         $('.node[id='+d.id+']').each(function(){
-//             console.log('path#'+$(this).attr("id"));
-//             $('path#'+$(this).attr("id")).fadeOut("fast").remove();
-//             $(this).fadeOut("fast", function(e){
-//                 this.remove();
-//             });
-//             delete_children($(this).attr("id"));
-//         });
-//         update();
-//     });
-// }
-
-// function delete_children(id) {
-//     $('.node[parent='+id+']').each(function(){
-//         $('path#'+$(this).attr("id")).fadeOut("fast").remove();
-//         $(this).fadeOut("fast", function(e){
-//             this.remove();
-//         });
-//         //delete_all_childs($(this).attr("id"));
-//     });
-// }
 
 function make_editable(d, field) {
     this.on("mouseover", function() {
